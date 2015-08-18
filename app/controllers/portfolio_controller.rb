cfa219eb85f243dd5a9b430dfb4c78c4d7218f1b
Rafael Malave <rafomalave@gmail.com>
2015-08-18 00:23:58 -0700

    Add testing gems and add portfolio controller and root page

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,29 @@
+class PortfolioController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def portfolio_params
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'portfolio/index'
+  resources :projects
+  root 'portfolio#index'
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +58,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
