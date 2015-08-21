2abcc47be3ef4c3d92a2ff08521b80783b78e8a5
Rafael Malave <rafomalave@gmail.com>
2015-08-21 00:40:56 -0700

    fix routes for singed_in

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/home',    to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about',   to: 'static_pages#about'
-  get '/sign_in' to: 'devise/sessions#new'
+  get '/sign_in', to: 'devise/sessions#new'
 
   get 'portfolio/index'
 
