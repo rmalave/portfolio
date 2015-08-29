0762a9369d6c3d639eb2c3c748647d525dfb07e1
Rafael Malave <rafomalave@gmail.com>
2015-08-28 23:15:58 -0700

    add login and logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
 class UsersController < ApplicationController
 
+
   def new
     @user = User.new
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params) 
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      login @user
+      flash[:success] = "Welcome to my portfolio site!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  root              'static_pages#home'
 
   resources :works
+  get   'home',     to: 'static_pages#home'
+  get   'contact',  to: 'static_pages#contact'
+  get   'about',    to: 'static_pages#about'
+  get   'signup',   to: 'users#new'
+  get   'portfolio/index'
+  get   'login',    to: 'sessions#new'
+  post  'login',    to: 'sessions#create'
+  delete 'logout',   to: 'sessions#destroy'
   resources :users
-  root 'static_pages#home'
-  get 'home',    to: 'static_pages#home'
-  get 'contact', to: 'static_pages#contact'
-  get 'about',   to: 'static_pages#about'
-  get 'signup', to: 'users#new'
-  get 'portfolio/index'
 
 
 end
